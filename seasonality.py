ce1fd7099e5fcff399a4ed0c9ebac1d1a08cd084
Martin Jul <martin@mjul.com>
2020-02-25 15:31:53 +0100

    Plot seasonality components

--- seasonality.py
+++ seasonality.py
@@ -9,6 +9,8 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 import seaborn as sns
 
+from graphics import plot_forecast, plot_components
+
 # For simplicity we use whole months
 num_months = 12
 
@@ -120,49 +122,6 @@ forecast_mean, forecast_scale, forecast_samples = (
     forecast_dist.stddev().numpy()[..., 0],
     forecast_dist.sample(num_samples).numpy()[..., 0])
 
-
-def plot_forecast(x, y,
-                  forecast_mean, forecast_scale, forecast_samples,
-                  title, x_locator=None, x_formatter=None):
-    """Plot a forecast distribution against the 'true' time series."""
-    colors = sns.color_palette()
-    c1, c2 = colors[0], colors[1]
-    fig = plt.figure(figsize=(12, 6))
-    ax = fig.add_subplot(1, 1, 1)
-
-    num_steps = len(y)
-    num_steps_forecast = forecast_mean.shape[-1]
-    num_steps_train = num_steps - num_steps_forecast
-
-    ax.plot(x, y, lw=2, color=c1, label='ground truth')
-
-    forecast_steps = np.arange(
-        x[num_steps_train],
-        x[num_steps_train] + num_steps_forecast,
-        dtype=x.dtype)
-
-    ax.plot(forecast_steps, forecast_samples.T, lw=1, color=c2, alpha=0.1)
-
-    ax.plot(forecast_steps, forecast_mean, lw=2, ls='--', color=c2,
-            label='forecast')
-    ax.fill_between(forecast_steps,
-                    forecast_mean - 2 * forecast_scale,
-                    forecast_mean + 2 * forecast_scale, color=c2, alpha=0.2)
-
-    ymin, ymax = min(np.min(forecast_samples), np.min(y)), max(np.max(forecast_samples), np.max(y))
-    yrange = ymax - ymin
-    ax.set_ylim([ymin - yrange * 0.1, ymax + yrange * 0.1])
-    ax.set_title("{}".format(title))
-    ax.legend()
-
-    if x_locator is not None:
-        ax.xaxis.set_major_locator(x_locator)
-        ax.xaxis.set_major_formatter(x_formatter)
-        fig.autofmt_xdate()
-
-    return fig, ax
-
-
 fig, ax = plot_forecast(
     dates, observations,
     forecast_mean, forecast_scale, forecast_samples,
@@ -176,3 +135,20 @@ ax.set_xlabel("Date")
 fig.autofmt_xdate()
 
 fig.show()
+
+# Build a dict mapping components to distributions over
+# their contribution to the observed signal.
+component_dists = tfp.sts.decompose_by_component(
+    model,
+    observed_time_series=observations,
+    parameter_samples=q_samples)
+
+component_means_, component_stddevs_ = (
+    {k.name: c.mean() for k, c in component_dists.items()},
+    {k.name: c.stddev() for k, c in component_dists.items()})
+
+_ = plot_components(dates, component_means_, component_stddevs_,
+                    # x_locator=co2_loc, x_formatter=co2_fmt
+                    )
+
+plt.show()
